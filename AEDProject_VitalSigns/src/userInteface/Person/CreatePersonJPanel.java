c61382f9d844833139c21251f9f4692f7364fdfc
ram-moodapally <ram.moodapally@gmail.com>
2021-11-02 23:46:55 -0400

    removed redundant  label

--- AEDProject_VitalSigns/src/userInteface/Person/CreatePersonJPanel.java
+++ AEDProject_VitalSigns/src/userInteface/Person/CreatePersonJPanel.java
@@ -17,7 +17,7 @@ import javax.swing.JPanel;
 
 /**
  *
- * @author akshay
+ * @author RamCharan
  */
 public class CreatePersonJPanel extends javax.swing.JPanel {
     
@@ -61,8 +61,6 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
         backJButton = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         personWeightJTextField = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        personCommunityJTextField = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         personHouseJTextField = new javax.swing.JTextField();
@@ -105,14 +103,6 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
             }
         });
 
-        jLabel3.setText("Community:");
-
-        personCommunityJTextField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                personCommunityJTextFieldActionPerformed(evt);
-            }
-        });
-
         jLabel4.setText("House:");
 
         jLabel5.setText("City:");
@@ -147,7 +137,6 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(patientNameJLabel)
                             .addComponent(jLabel2)
-                            .addComponent(jLabel3)
                             .addComponent(jLabel4)
                             .addComponent(jLabel5)
                             .addComponent(jLabel6)
@@ -155,24 +144,23 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
                                 .addContainerGap()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(AgeJLabel)
-                                    .addComponent(jLabel7))))
+                                    .addComponent(jLabel7, javax.swing.GroupLayout.Alignment.TRAILING))))
                         .addGap(235, 235, 235)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(personAgeJTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
                             .addComponent(pesonNameJTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
                             .addComponent(personWeightJTextField)
-                            .addComponent(personCommunityJTextField)
                             .addComponent(personHouseJTextField)
                             .addComponent(personCityJTextField)
                             .addComponent(personSSNJTextField)
                             .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap(25, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
-                .addGap(98, 98, 98)
+                .addGap(89, 89, 89)
                 .addComponent(backJButton, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(38, 38, 38)
                 .addComponent(addPersonJButton)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -197,10 +185,6 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(personCommunityJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(personHouseJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -211,11 +195,11 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel7))
-                .addGap(41, 41, 41)
+                .addGap(39, 39, 39)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(backJButton)
                     .addComponent(addPersonJButton))
-                .addContainerGap(327, Short.MAX_VALUE))
+                .addContainerGap(361, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -250,7 +234,7 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
         personHouseJTextField.setText("");
         personCityJTextField.setText("");
         personSSNJTextField.setText("");
-        personCommunityJTextField.setText("");
+        //personCommunityJTextField.setText("");
     }
     private boolean validateSSN(String ssnString){
         if(ssn.contains(ssnString))
@@ -283,10 +267,6 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_personWeightJTextFieldActionPerformed
 
-    private void personCommunityJTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_personCommunityJTextFieldActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_personCommunityJTextFieldActionPerformed
-
     private void personSSNJTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_personSSNJTextFieldActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_personSSNJTextFieldActionPerformed
@@ -306,7 +286,6 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
@@ -314,7 +293,6 @@ public class CreatePersonJPanel extends javax.swing.JPanel {
     private javax.swing.JLabel patientNameJLabel;
     private javax.swing.JTextField personAgeJTextField;
     private javax.swing.JTextField personCityJTextField;
-    private javax.swing.JTextField personCommunityJTextField;
     private javax.swing.JTextField personHouseJTextField;
     private javax.swing.JTextField personSSNJTextField;
     private javax.swing.JTextField personWeightJTextField;
